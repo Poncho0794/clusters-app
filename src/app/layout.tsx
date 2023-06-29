ad1361b138ef1d134c6960bc64061e94cc6ce69e
Alfonso Banda <alfonso@yalochat.com>
2023-06-28 18:11:59 -0600

    Init project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+//import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <h1>Layout</h1>
+
+        {children}
+        </body>
     </html>
   )
 }
