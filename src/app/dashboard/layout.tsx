2f09b1c6dbf10a4665824e7c5bba8bb8b9d1b717
Alfonso Banda <abanda@apexsystems.com>
2023-07-04 14:30:26 -0600

    Change export of authOptions

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/Navbar"
-import { GetServerSideProps } from "next"
+import { authOptions } from "@/helpers/authOptions";
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "../api/auth/[...nextauth]/route"
 import { redirect } from 'next/navigation';
 
 export default async function Layout({ children }: {
